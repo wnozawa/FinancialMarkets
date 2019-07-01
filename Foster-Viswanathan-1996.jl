9b413deee27563c7dfcb3fde0e4972157507cfe7
Wataru Nozawa <wnwnwnwnwnwn@gmail.com>
2019-07-02 01:20:10 +0900

    put comments; number of periods increased to N+1

--- Foster-Viswanathan-1996.jl
+++ Foster-Viswanathan-1996.jl
@@ -38,26 +38,26 @@ d_N = 0
 
 
 # # variables for storing solution
-# a = zeros(1,N)
-# ps = zeros(1,N)
-# m = zeros(1,N)
-# d = zeros(1,N)
-# l = zeros(1,N)
-# g = zeros(1,N)
-# b = zeros(1,N)
-# d = zeros(1,N)
-# eta = zeros(1,N)
-# ph = zeros(1,N)
-# L = zeros(1,N)
-# O = zeros(1,N)
-# S = zeros(1,N)
+# a = zeros(1,N+1)
+# ps = zeros(1,N+1)
+# m = zeros(1,N+1)
+# d = zeros(1,N+1)
+# l = zeros(1,N+1)
+# g = zeros(1,N+1)
+# b = zeros(1,N+1)
+# d = zeros(1,N+1)
+# eta = zeros(1,N+1)
+# ph = zeros(1,N+1)
+# L = zeros(1,N+1)
+# O = zeros(1,N+1)
+# S = zeros(1,N+1)
 
 #--- Intial guess for L_N
 
-L[N] = S_0/theta^2 + chi*(M-1)/M
-
-O[N] = L[N] - chi
-S[N] = (L[N] + (M-1)*O[N])*theta^2/M
+# L[N] = S_0/theta^2 + chi*(M-1)/M
+#
+# O[N] = L[N] - chi
+# S[N] = (L[N] + (M-1)*O[N])*theta^2/M
 
 
 # solve
@@ -75,17 +75,21 @@ end
 
 get_beta(l_n, S_n) = l_n*theta*sv^2/(M*S_n)  # third equation in Proposition 1
 
-get_phi(O_n, l_n, b_n, L_n) = (O_n + (l_n*b_n/theta)*chi)/(L_n - (l_n*b_n/theta)*(M-1)*chi)  # equation (35)
+get_phi(O_n, l_n, b_n, L_n) = (O_n + (l_n*b_n/theta)*chi)/(L_n - (l_n*b_n/theta)*(M-1)*chi)
+    # equation (35)
 
-get_gamma(m_n, l_n, b_n) = (1-2*m_n*l_n)*(1-(l_n*b_n*(M-1)/theta))/(2*l_n*(1-m_n*l_n))  # second equation in Proposition 1
-get_eta(phi_n) = theta/M*(1+(M-1)*phi_n)
+get_gamma(m_n, l_n, b_n) = (1-2*m_n*l_n)*(1-(l_n*b_n*(M-1)/theta))/(2*l_n*(1-m_n*l_n))
+    # second equation in Proposition 1
+get_eta(phi_n) = theta/M*(1+(M-1)*phi_n)  # 9th equation in Proposition 1
 
-get_alpha(eta_n, l_n, b_n, a_n, ph_n) = (eta_n - l_n*b_n*(1+(M-1)*ph_n))*b_n + a_n*(1 - (l_n*b_n/theta)*(1+ph_n*(M-1)))^2
-get_psi(eta_n, l_n, b_n, a_n, ph_n, gamma_n, psi_n) = (eta_n - l_n*b_n*(1+(M-1)*ph_n))*gamma_n - l_n*gamma_n*b_n + b_n*(1-l_n*b_n*(M-1)/theta) + psi_n*(1-l_n*b_n*(1+(M-1)*ph_n)/theta)*(1-l_n*b_n*(M-1)/theta - l_n*gamma_n)
-get_mu(l_n, gamma_n, b_n, m_n) = -l_n*gamma_n^2 + gamma_n*(1-l_n*b_n*(M-1)/theta) + m_n*(1-l_n*b_n*(M-1)/theta - l_n*gamma_n)^2
+get_alpha(eta_n, l_n, b_n, a_n, ph_n) = (eta_n - l_n*b_n*(1+(M-1)*ph_n))*b_n + a_n*(1 - (l_n*b_n/theta)*(1+ph_n*(M-1)))^2  # 4th equation in Proposition 1
 
-get_L(L_n, l_n, b_n) = (L_n - (l_n*b_n/theta)*(M-1)*chi)/(1-M*l_n*b_n/theta)
-get_O(O_n, l_n, b_n) = (O_n + (l_n*b_n/theta)*chi)/(1-(M*l_n*b_n/theta))
+get_psi(eta_n, l_n, b_n, a_n, ph_n, gamma_n, psi_n) = (eta_n - l_n*b_n*(1+(M-1)*ph_n))*gamma_n - l_n*gamma_n*b_n + b_n*(1-l_n*b_n*(M-1)/theta) + psi_n*(1-l_n*b_n*(1+(M-1)*ph_n)/theta)*(1-l_n*b_n*(M-1)/theta - l_n*gamma_n)  # 5th equation in Proposition 1
+
+get_mu(l_n, gamma_n, b_n, m_n) = -l_n*gamma_n^2 + gamma_n*(1-l_n*b_n*(M-1)/theta) + m_n*(1-l_n*b_n*(M-1)/theta - l_n*gamma_n)^2  # 6th equation in Proposition 1
+
+get_L(L_n, l_n, b_n) = (L_n - (l_n*b_n/theta)*(M-1)*chi)/(1-M*l_n*b_n/theta)  # equation (33)
+get_O(O_n, l_n, b_n) = (O_n + (l_n*b_n/theta)*chi)/(1-(M*l_n*b_n/theta))  # equation (34)
 
 function get_delta(d_n, a_n, l_n, b_n, L_n_1, O_n_1)
     Var = (L_n_1^2 - O_n_1^2)/L_n_1
@@ -98,25 +102,25 @@ end
 
 function solve_system(L_N)
     # variables for storing solution
-    a = zeros(1,N)
-    ps = zeros(1,N)
-    m = zeros(1,N)
-    d = zeros(1,N)
-    l = zeros(1,N)
-    g = zeros(1,N)
-    b = zeros(1,N)
-    d = zeros(1,N)
-    eta = zeros(1,N)
-    ph = zeros(1,N)
-    L = zeros(1,N)
-    O = zeros(1,N)
-    S = zeros(1,N)
+    a = zeros(1,N+1)
+    ps = zeros(1,N+1)
+    m = zeros(1,N+1)
+    d = zeros(1,N+1)
+    l = zeros(1,N+1)
+    g = zeros(1,N+1)
+    b = zeros(1,N+1)
+    d = zeros(1,N+1)
+    eta = zeros(1,N+1)
+    ph = zeros(1,N+1)
+    L = zeros(1,N+1)
+    O = zeros(1,N+1)
+    S = zeros(1,N+1)
 
-    L[N] = L_N
-    O[N] = L_N - chi
-    S[N] = (L_N + (M-1)*O[N])*theta^2/M
+    L[N+1] = L_N
+    O[N+1] = L_N - chi
+    S[N+1] = (L_N + (M-1)*O[N+1])*theta^2/M
 
-    for n in [N:-1:2;]
+    for n in [N+1:-1:2;]
         # compute lam_n from S_n & a_n
         eq_l(l) = eq_l_n(l, S[n], ps[n], L[n])
         l[n] = find_zero(eq_l, (0, S[n]^(1/2)/sv))
@@ -132,15 +136,56 @@ function solve_system(L_N)
 
         L[n-1] = get_L(L[n], l[n], b[n])
         O[n-1] = get_O(O[n], l[n], b[n])
-        S[n-1] = (L[n-1] + (M-1)*O[n-1])*theta^2/M
+        S[n-1] = (L[n-1] + (M-1)*O[n-1])*theta^2/M  # equation (8)
         d[n-1] = get_delta(d[n], a[n], l[n], b[n], L[n-1], O[n-1])
     end
     return L[1] - L_0
 end
 
-# solve
-L_N_eqm = find_zero(solve_system, (0, 5))
-## draw figures
+#--- solve
+L_N_eqm = find_zero(solve_system, (0.133334, 10))
+
+
+#--- get solution
+a = zeros(1,N+1)
+ps = zeros(1,N+1)
+m = zeros(1,N+1)
+d = zeros(1,N+1)
+l = zeros(1,N+1)
+g = zeros(1,N+1)
+b = zeros(1,N+1)
+d = zeros(1,N+1)
+eta = zeros(1,N+1)
+ph = zeros(1,N+1)
+L = zeros(1,N+1)
+O = zeros(1,N+1)
+S = zeros(1,N+1)
+
+L[N+1] = L_N
+O[N+1] = L_N - chi
+S[N+1] = (L_N + (M-1)*O[N+1])*theta^2/M
+
+for n in [N+1:-1:2;]
+    # compute lam_n from S_n & a_n
+    eq_l(l) = eq_l_n(l, S[n], ps[n], L[n])
+    l[n] = find_zero(eq_l, (0, S[n]^(1/2)/sv))
+
+    b[n] = get_beta(l[n], S[n])
+    ph[n] = get_phi(O[n], l[n], b[n], L[n])
+    g[n] = get_gamma(m[n], l[n], b[n])
+    eta[n] = get_eta(ph[n])
+
+    a[n-1] = get_alpha(eta[n], l[n], b[n], a[n], ph[n])
+    ps[n-1] = get_psi(eta[n], l[n], b[n], a[n], ph[n], g[n], ps[n])
+    m[n-1] = get_mu(l[n], g[n], b[n], m[n])
+
+    L[n-1] = get_L(L[n], l[n], b[n])
+    O[n-1] = get_O(O[n], l[n], b[n])
+    S[n-1] = (L[n-1] + (M-1)*O[n-1])*theta^2/M  # equation (8)
+    d[n-1] = get_delta(d[n], a[n], l[n], b[n], L[n-1], O[n-1])
+end
+
+#--- draw figures
 
 plot(l[2:N]')
 
